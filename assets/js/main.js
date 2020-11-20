e667d1b671df4c4cf4c0356dc7e71971b53af1ea
Varsha C Bendre <varsha6319@gmail.com>
2020-11-20 23:11:07 +1100

    Bug fixed in main.js for button style

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,13 @@ window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) 
+  {
+    mybutton.style.display = 'block';
+  } 
+  else 
+  {
+    mybutton.style.display = 'none';
   }
 }
 // When the user clicks on the button, scroll to the top of the document
